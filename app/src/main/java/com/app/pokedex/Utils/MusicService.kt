acb86d250561e53e0d5c1691551323eb84b82d3e
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2025-02-09 21:36:10 -0300

    Update MusicService.kt

--- app/src/main/java/com/app/pokedex/Utils/MusicService.kt
+++ app/src/main/java/com/app/pokedex/Utils/MusicService.kt
@@ -11,29 +11,24 @@ class MusicService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        // Verifique se o arquivo de música existe no diretório raw
         mediaPlayer = MediaPlayer.create(this, R.raw.backgroundsound)
-        mediaPlayer?.isLooping = true // Opcional: para tocar a música em loop
+        mediaPlayer?.isLooping = true
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        // Se o mediaPlayer não for nulo, começa a tocar
         mediaPlayer?.let {
             if (!it.isPlaying) {
                 it.start()
             }
         }
-        // Retorna START_STICKY para que o serviço continue em execução se o sistema precisar reiniciar
         return START_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        // Não precisamos de binding, portanto retornamos null
         return null
     }
 
     override fun onDestroy() {
-        // Libera o recurso do mediaPlayer quando o serviço for destruído
         mediaPlayer?.apply {
             stop()
             release()
